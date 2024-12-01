3cda01f7606a653cd7b76649729794839b0c1d8e
TomaDumitrescu <ti.dumitrescu@gmail.com>
2024-12-01 13:11:09 +0200

    Implemented and tested map operation

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,63 @@
 #include "main.h"
 
-using namespace std;
-
 // Deallocates resource
-inline void clean(ifstream &fin, vector<string> *files)
+inline void clean(ifstream &fin, vector<string> *files, unordered_map<string, set<int>> *thread_maps)
 {
     fin.close();
     delete files;
+    delete[] thread_maps;
 }
 
+// Each thread will have each dictionary to put pairs <string, int>
 void *map(void *arg)
 {
+    mapper_arg_t *m_arg = (mapper_arg_t *)arg;
+    int fid = -1, len, cnt;
+    char buffer[WORD_LEN] = {0}, word[WORD_LEN] = {0};
+    while (true) {
+        // Avoid RW race conditions
+        pthread_mutex_lock(m_arg->lock);
+            if (m_arg->tasks->size() == 0) {
+                fid = -1;
+            } else {
+                fid = m_arg->tasks->front();
+                m_arg->tasks->pop();
+            }
+        pthread_mutex_unlock(m_arg->lock);
+
+        if (fid == -1) {
+            break;
+        }
+
+        // Process the file
+        ifstream in((*m_arg->files)[fid].c_str());
+
+        while (in >> buffer) {
+            len = strlen(buffer), cnt = 0;
+            // Standardizing the word
+            for (int i = 0; i < len; i++) {
+                if (buffer[i] >= 'a' && buffer[i] <= 'z') {
+                    word[cnt++] = buffer[i];
+                } else if (buffer[i] >= 'A' && buffer[i] <= 'Z') {
+                    word[cnt++] = (char)(buffer[i] - 'A' + 'a');
+                }
+            }
+
+            word[cnt] = '\0';
+
+            if (word[0] == '\0') {
+                continue;
+            }
+
+            (*m_arg->thread_dict)[string(word)].insert(fid + 1);
+            memset(buffer, '\0', len + 1);
+        }
+
+        in.close();
+    }
+
+    // Useful barrier for the reducers
+    //pthread_barrier_wait(m_arg->barrier);
     return NULL;
 }
 
@@ -24,21 +71,17 @@ int main(int argc, char **argv)
     // Loading data
     ifstream fin(argv[3]);
 
-    int num_files, total_dim = 0;
+    int num_files;
     fin >> num_files;
 
-    vector<int> fdim(num_files, 0);
     vector<string> *files = new vector<string>;
+    queue<int> tasks;
     char buffer[FILE_LEN] = {0};
     fin.getline(buffer, FILE_LEN);
     for (int i = 0; i < num_files; ++i) {
         fin.getline(buffer, FILE_LEN);
         files->push_back(string(buffer));
-
-        struct stat st;
-        stat(buffer, &st);
-        fdim[i] = st.st_size;
-        total_dim += fdim[i];
+        tasks.push(i);
     }
 
     // Creating threads
@@ -48,8 +91,18 @@ int main(int argc, char **argv)
     mapper_arg_t map_args[mappers];
     reducer_arg_t red_args[reducers];
 
+    pthread_mutex_t lock;
+    DIE(pthread_mutex_init(&lock, NULL) != 0, "Mutex initialization failed!\n");
+
+    pthread_barrier_t barrier;
+    pthread_barrier_init(&barrier, NULL, mappers + reducers);
+
+    unordered_map<string, set<int>> *thread_maps = new unordered_map<string, set<int>>[mappers]();
     for (id = 0; id < mappers + reducers; ++id) {
         if (id < mappers) {
+            map_args[id].thread_dict = &thread_maps[id], map_args[id].tasks = &tasks;
+            map_args[id].files = files, map_args[id].lock = &lock;
+            map_args[id].barrier = &barrier;
             DIE(pthread_create(&threads[id], NULL, map, (void *) &map_args[id]), "pth_create");
         } else {
             DIE(pthread_create(&threads[id], NULL, reduce, (void *) &red_args[id - mappers]), "pth_create");
@@ -61,6 +114,7 @@ int main(int argc, char **argv)
         DIE(pthread_join(threads[id], &status), "pthread_join");
     }
 
-    clean(fin, files);
+    // Cleaning dynamical allocated resources
+    clean(fin, files, thread_maps);
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -4,10 +4,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstring>
 #include <string>
-#include <sys/stat.h>
 #include <pthread.h>
+#include <queue>
+#include <unordered_map>
 #include <errno.h>
+#include <set>
+
+using namespace std;
 
 
 #define DIE(assertion, call_description)            \
@@ -23,9 +28,14 @@
     } while (0)
 
 #define FILE_LEN 80
+#define WORD_LEN 50
 
 struct mapper_arg_t {
-
+    unordered_map<string, set<int>> *thread_dict;
+    queue<int> *tasks;
+    vector<string> *files;
+    pthread_mutex_t *lock;
+    pthread_barrier_t *barrier;
 };
 
 struct reducer_arg_t {
